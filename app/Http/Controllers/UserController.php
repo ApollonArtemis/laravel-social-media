3fef9edbd2f30c608e6f4995e0cd260da605e8ae
ApollonArtemis <kyle.gabilagon06@gmail.com>
2026-02-27 11:59:56 +0800

    Add UserController http/controllers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,12 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     //
-    public function register() {
+    public function register(Request $request) {
+        $incomingFields = $request->validate([
+            'fullname' => ['required', 'max:30'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'max:16', 'min:8']
+        ]);
         return 'Hello from our controller';
     }
 }
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -179,15 +179,15 @@
             <img class="mb-4" src="{{ asset('assets/brand/bootstrap-logo.svg') }}" alt="" width="72" height="57" />
             <h1 class="h3 mb-3 fw-normal">Registration</h1>
             <div class="form-floating">
-                <input type="text" class="form-control" id="floatingName" placeholder="John Doe" />
+                <input type="text" class="form-control" id="floatingName" placeholder="John Doe" name="fullname"/>
                 <label for="floatingName">Full Name</label>
             </div>
             <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email"/>
                 <label for="floatingInput">Email address</label>
             </div>
             <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password"/>
                 <label for="floatingPassword">Password</label>
             </div>
             <div class="form-check text-start my-3">
@@ -196,7 +196,7 @@
                     Remember me
                 </label>
             </div>
-            <button class="btn btn-primary w-100 py-2" type="submit">
+            <button class="btn btn-primary w-100 py-2">
                 Register
             </button>
             <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2025</p>
